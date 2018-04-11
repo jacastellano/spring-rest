eea86ed461953f244a115fe9a269cbfd1f5bf79f
José A. Castellano <jacastellanoa@gmail.com>
2018-04-11 12:33:58 +0200

    Adding new controllers

--- quick-poll/src/main/java/com/jacastellano/quickpoll/controller/PollController.java
+++ quick-poll/src/main/java/com/jacastellano/quickpoll/controller/PollController.java
@@ -1,12 +1,19 @@
 package com.jacastellano.quickpoll.controller;
 
+import java.net.URI;
+import java.util.Optional;
+
 import javax.inject.Inject;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jacastellano.quickpoll.domain.Poll;
 import com.jacastellano.quickpoll.repository.PollRepository;
@@ -17,9 +24,41 @@ public class PollController {
 	@Inject
 	private PollRepository pollRepository;
 
-	@RequestMapping(value="/polls", method=RequestMethod.GET)
+	@RequestMapping(value = "/polls", method = RequestMethod.GET)
 	public ResponseEntity<Iterable<Poll>> getAllPolls() {
 		Iterable<Poll> allPolls = pollRepository.findAll();
 		return new ResponseEntity<>(allPolls, HttpStatus.OK);
 	}
+
+	@RequestMapping(value = "/polls", method = RequestMethod.POST)
+	public ResponseEntity<?> createPoll(@RequestBody Poll poll) {
+
+		poll = pollRepository.save(poll);
+
+		HttpHeaders responseHeaders = new HttpHeaders();
+		URI newPollUri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(poll.getId())
+				.toUri();
+		responseHeaders.setLocation(newPollUri);
+
+		return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
+	}
+
+	@RequestMapping(value = "/polls/{pollId}", method = RequestMethod.GET)
+	public ResponseEntity<?> getPoll(@PathVariable Long pollId) {
+		Optional<Poll> p = pollRepository.findById(pollId);
+		return new ResponseEntity<>(p, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/polls/{pollId}", method = RequestMethod.PUT)
+	public ResponseEntity<?> updatePoll(@RequestBody Poll poll, @PathVariable Long pollId) {
+		pollRepository.save(poll);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/polls/{pollId}", method = RequestMethod.DELETE)
+	public ResponseEntity<?> deletePoll(@PathVariable Long pollId) {
+		pollRepository.deleteById(pollId);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
 }
---
+++ quick-poll/src/main/java/com/jacastellano/quickpoll/dto/OptionCount.java
@@ -0,0 +1,30 @@
+package com.jacastellano.quickpoll.dto;
+
+public class OptionCount {
+
+	private Long optionId;
+
+	private int count;
+
+	public Long getOptionId() {
+		return optionId;
+	}
+
+	public void setOptionId(Long optionId) {
+		this.optionId = optionId;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	@Override
+	public String toString() {
+		return "OptionCount [optionId=" + optionId + ", count=" + count + "]";
+	}
+
+}
